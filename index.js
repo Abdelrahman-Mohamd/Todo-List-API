b17f5f5b4f939043d3184656cb6768506fdf36a7
abdelrahman <abdelrahman181181@yahoo.com>
2024-07-23 18:20:57 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+let tasks = [
+  { id: 1, task: "do your homework" },
+  { id: 2, task: "brush your teeth" },
+  { id: 3, task: "take a shower" },
+  { id: 4, task: "eat breakfast" },
+  { id: 5, task: "watch a movie" },
+];
+
+// Endpoint to get all tasks
+app.get("/getAllTasks", (req, res) => {
+  res.json(tasks);
+});
+
+// Endpoint to get a specific task using id
+app.get("/task/:id", (req, res) => {
+  const task = tasks.find((t) => t.id == req.params.id);
+  if (task) {
+    res.json(task);
+  } else {
+    res.status(404).send("Task not found");
+  }
+});
+
+// Endpoint to add a new task
+app.post("/tasks", (req, res) => {
+  const { task } = req.body;
+  if (task) {
+    const newTask = { id: tasks.length + 1, task };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+  } else {
+    res.status(400).send("Task description is required");
+  }
+});
+
+// Endpoint to delete a task using id
+app.delete("/task/:id", (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const taskIndex = tasks.findIndex((t) => t.id === taskId);
+  if (taskIndex !== -1) {
+    tasks.splice(taskIndex, 1);
+    res.status(200).send(`Task with id ${taskId} deleted`);
+  } else {
+    res.status(404).send("Task not found");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Mock API running at http://localhost:${port}`);
+});
